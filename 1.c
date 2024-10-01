4de29e2bbd55febd8656a6bea723c5af8e65a44f
yagnik529 <yagnikpansuriya603@gmail.com>
2024-10-01 18:09:09 +0530

    lab 11

---
+++ 1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+void main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    // 1
+    // int count = 0;
+    // for (int i = 1; i <= 5; i++)
+    // {
+
+    //     count++;
+    // }
+    // printf("%d", count);
+    // 2
+    // float count = 0;
+    // for (int i = 0; i < 5; i++)
+    // {
+
+    //     count++;
+    // }
+    // // printf("%d", count);
+    // float sum = 0;
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     sum = sum + arr[i];
+    // }
+    // printf("%f", sum);
+    // printf("%f", count);
+    // float avg = 0.0;
+    // avg = sum / count;
+    // printf(" %f ", avg);
+
+    // 3
+
+    // int a1[5] = {1, 2, 3, 4, 5}, a2[5] = {4, 5, 6, 1, 2};
+    // int a3[5];
+    // int i;
+    // for (i = 0; i < 5; i++)
+    // {
+    //     a3[i] = a1[i] + a2[i];
+    // }
+    // for (i = 0; i < 5; i++)
+    // {
+    //     printf("%d", a3[i]);
+    // }
+}
